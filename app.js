33e12fb89770da364a70fe3e0092c955432ec9ee
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-06-16 11:15:25 +0100

    added index view with ejs templating

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res){
-  res.send('Hello World.');
+  res.render('index', { header: 'index page' });
 });
 
 app.listen(port, function(){
