68b59d389eea00b15140bde0c98f47b921b110f6
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-06-16 11:35:01 +0100

    logging middleware

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
+var morgan = require('morgan');
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+app.use(morgan('dev')); // alternative to below
+
+// Logging Middleware - btw request and response
+// app.use(function(req, res, next){
+//   console.log('%s request to %s from %s', req.method, req.path, req.ip);
+//   next();
+// })
+
 app.get('/', function(req, res){
-  res.render('index', { header: 'index page' });
+  res.render('index', { header: 'index page', name: 'Lauren' });
 });
 
 app.listen(port, function(){
